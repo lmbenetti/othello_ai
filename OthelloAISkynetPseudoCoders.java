bebde735a8107f6bde71cd947e4f63a097639e4e
Lisandro Marco Benetti <limb@itu.dk>
2024-03-14 17:44:23 +0100

    bug solved

--- OthelloAISkynetPseudoCoders.java
+++ OthelloAISkynetPseudoCoders.java
@@ -10,8 +10,9 @@ public class OthelloAISkynetPseudoCoders implements IOthelloAI {
     }
 
     public Position decideMove(GameState s) {
+        System.out.println("decision made");
         int count = s.countTokens()[0] + s.countTokens()[1];
-        depp = count - 4 + 10;
+        depp = count - 4 + 12;
         if (count == 4) {
             firstPlayer = true;
         }
@@ -19,6 +20,7 @@ public class OthelloAISkynetPseudoCoders implements IOthelloAI {
         return maxValue(s, Integer.MIN_VALUE, Integer.MAX_VALUE).getPosition();
     }
 
+    
     public Tuple maxValue(GameState s, int alpha, int beta) {
         Tuple tupleUtilMove = new Tuple(Integer.MIN_VALUE, new Position(-10, -10));
 
@@ -27,51 +29,77 @@ public class OthelloAISkynetPseudoCoders implements IOthelloAI {
             return tupleUtilMove;
         } else {
             ArrayList<Position> moves = s.legalMoves();
-
-            for (Position action : moves) {
+            if (moves.isEmpty()){
                 GameState tempBoard = new GameState(s.getBoard(), s.getPlayerInTurn());
-                tempBoard.insertToken(action);
-                Tuple tuple2 = minValue(tempBoard, alpha, beta);
-                tuple2.setPosition(action);
-
+                tempBoard.changePlayer();
+                Tuple tuple2 = maxValue(tempBoard, beta, alpha);
                 if (tuple2.compareTo(tupleUtilMove) > 0) {
                     tupleUtilMove = tuple2;
                     alpha = Math.max(alpha, tupleUtilMove.getUtility());
                 }
-
                 if (tupleUtilMove.getUtility() >= beta) {
                     return tupleUtilMove;
                 }
+            }
+            else {
+                for (Position action : moves) {
+                    GameState tempBoard = new GameState(s.getBoard(), s.getPlayerInTurn());
+                    tempBoard.insertToken(action);
+                    Tuple tuple2 = minValue(tempBoard, alpha, beta);
+                    tuple2.setPosition(action);
 
+                    if (tuple2.compareTo(tupleUtilMove) > 0) {
+                        tupleUtilMove = tuple2;
+                        alpha = Math.max(alpha, tupleUtilMove.getUtility());
+                    }
+
+                    if (tupleUtilMove.getUtility() >= beta) {
+                        return tupleUtilMove;
+                    }
+
+                }
             }
         }
         return tupleUtilMove;
     }
 
     public Tuple minValue(GameState s, int alpha, int beta) {
-        Tuple tupleUtilMove = new Tuple(Integer.MAX_VALUE, new Position(-11, -11));
+        Tuple tupleUtilMove = new Tuple(Integer.MAX_VALUE, new Position(-15, -15));
 
         if (s.isFinished() || s.countTokens()[0] + s.countTokens()[1] >= depp) {
             tupleUtilMove.updateUtility(s, firstPlayer);
             return tupleUtilMove;
         } else {
             ArrayList<Position> moves = s.legalMoves();
-
-            for (Position action : moves) {
+            if (moves.isEmpty()){
                 GameState tempBoard = new GameState(s.getBoard(), s.getPlayerInTurn());
-                tempBoard.insertToken(action);
-                Tuple tuple2 = maxValue(tempBoard, alpha, beta);
-                tuple2.setPosition(action);
-
+                tempBoard.changePlayer();
+                Tuple tuple2 = minValue(tempBoard, alpha, beta);
                 if (tuple2.compareTo(tupleUtilMove) < 0) {
                     tupleUtilMove = tuple2;
                     beta = Math.min(beta, tupleUtilMove.getUtility());
                 }
-
                 if (tupleUtilMove.getUtility() <= alpha) {
                     return tupleUtilMove;
                 }
+            }
+            else {
+                
+                for (Position action : moves) {
+                    GameState tempBoard = new GameState(s.getBoard(), s.getPlayerInTurn());
+                    tempBoard.insertToken(action);
+                    Tuple tuple2 = maxValue(tempBoard, alpha, beta);
+                    tuple2.setPosition(action);
 
+                    if (tuple2.compareTo(tupleUtilMove) < 0) {
+                        tupleUtilMove = tuple2;
+                        beta = Math.min(beta, tupleUtilMove.getUtility());
+                    }
+
+                    if (tupleUtilMove.getUtility() <= alpha) {
+                        return tupleUtilMove;
+                    }
+                }
             }
         }
         return tupleUtilMove;
